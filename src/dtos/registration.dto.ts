fb30ec67279dd205f23f76b8601bbba95800d691
jadegnew <dmytro.khabatilin@gmail.com>
2023-02-19 17:30:35 +0300

    added data validation

--- src/dtos/registration.dto.ts
+++ src/dtos/registration.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
 export class RegistrationDto {
+
+    @IsEmail()
     email: string;
+    
+    @IsString()
+    @IsNotEmpty()
     username: string;
+    
+    @IsString()
+    @MinLength(7)
     password: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookie_parser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe);
   app.use(cookie_parser());
   await app.listen(Number(process.env.PORT), () => {
     console.log(`Server started listening on ${process.env.HOST}:${process.env.PORT}`);
